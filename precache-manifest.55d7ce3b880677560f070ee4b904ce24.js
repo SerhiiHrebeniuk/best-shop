c75c931ee7ae36a2e7cff662f0cacf8d96c9ccef
unknown <sarmat.p8@gmail.com>
2020-10-02 01:25:02 +0200

    Updates

---
+++ precache-manifest.55d7ce3b880677560f070ee4b904ce24.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566e9f6e8d49a00fb57b018ef0450961",
+    "url": "/best-shop/index.html"
+  },
+  {
+    "revision": "9399440bf2867810246e",
+    "url": "/best-shop/static/css/main.679ac284.chunk.css"
+  },
+  {
+    "revision": "38e5ec66e870de1f4d34",
+    "url": "/best-shop/static/js/2.a429dd30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/best-shop/static/js/2.a429dd30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9399440bf2867810246e",
+    "url": "/best-shop/static/js/main.7fc93548.chunk.js"
+  },
+  {
+    "revision": "c0dd290aa3a4bfb7a619",
+    "url": "/best-shop/static/js/runtime-main.8dcc423e.js"
+  },
+  {
+    "revision": "9d9c39362722c2ae0da68ffb08057cb2",
+    "url": "/best-shop/static/media/Background.9d9c3936.png"
+  },
+  {
+    "revision": "524c4e508a3a5cfb01cedd543c066050",
+    "url": "/best-shop/static/media/Catalog.524c4e50.svg"
+  },
+  {
+    "revision": "62e617665c9c88a4fd453020f1730ae6",
+    "url": "/best-shop/static/media/Ckechmark-1.62e61766.svg"
+  },
+  {
+    "revision": "0bbd388638c043fe33a85dfc07bfd190",
+    "url": "/best-shop/static/media/Close.0bbd3886.svg"
+  },
+  {
+    "revision": "e60d48c9da2336a808bc5dbe6609370b",
+    "url": "/best-shop/static/media/Eye.e60d48c9.svg"
+  },
+  {
+    "revision": "fc1299592b84317c9685ebb6c442f6e8",
+    "url": "/best-shop/static/media/Facebook.fc129959.svg"
+  },
+  {
+    "revision": "96b01900b4b0487820ce21f91e044d89",
+    "url": "/best-shop/static/media/Macbook.96b01900.png"
+  },
+  {
+    "revision": "e6c52bb548da132232932de83585a92e",
+    "url": "/best-shop/static/media/Mail Icon.e6c52bb5.svg"
+  },
+  {
+    "revision": "0185a71ef02153f3c6fa9cee46bb7537",
+    "url": "/best-shop/static/media/Person.0185a71e.svg"
+  },
+  {
+    "revision": "68325f85905a0d27b47d7f900c776976",
+    "url": "/best-shop/static/media/Phone Icon.68325f85.svg"
+  },
+  {
+    "revision": "f5064fc30470293506e8bfa3cf2041ac",
+    "url": "/best-shop/static/media/Trumpet.f5064fc3.png"
+  },
+  {
+    "revision": "5bb39e9fb75e0a3b980733742fdb0db9",
+    "url": "/best-shop/static/media/Twitter.5bb39e9f.svg"
+  },
+  {
+    "revision": "74ed7d460e853d011847d81869e7f899",
+    "url": "/best-shop/static/media/iPhone.74ed7d46.png"
+  }
+]);
